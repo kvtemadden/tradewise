3747b0cf0ee2070cd0232583f42346f34a746583
kate <katy.madden99@gmail.com>
2021-03-29 22:57:22 +0100

    initial commit, mvc layout / models / server / connection set up

---
+++ models/index.js
@@ -0,0 +1,46 @@
+const User = require('./User');
+const Job = require('./Job');
+const Comment = require('./Comment');
+const Role = require('./Role');
+
+Role.hasOne(User, {
+  foreignKey: 'role_id',
+  onDelete: 'CASCADE'
+});
+
+User.belongsTo(Role, {
+    foreignKey: 'role_id',
+    onDelete: 'CASCADE'
+});
+
+User.hasMany(Job, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Job.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: "cascade"
+});
+
+Comment.belongsTo(Job, {
+  foreignKey: 'blog_id',
+  onDelete: "cascade"
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: "cascade"
+});
+
+Job.hasMany(Comment, {
+  foreignKey: 'blog_id',
+  onDelete: "cascade"
+})
+
+module.exports = { User, Job, Comment };
---
+++ utils/helpers.js
@@ -0,0 +1,8 @@
+module.exports = {
+    format_date: (date) => {
+      return `${new Date(date).getDate()}/${new Date(date).getMonth() + 1}/${
+        new Date(date).getFullYear()
+      }`;
+    }
+  };
+  
