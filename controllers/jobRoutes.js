eaf9fef752748ec1ad7408f886c328d7dc129eab
kate <katy.madden99@gmail.com>
2021-03-31 21:01:18 +0100

    added delete route for job post

--- controllers/jobRoutes.js
+++ controllers/jobRoutes.js
@@ -13,15 +13,43 @@ router.get('/jobs/new', withAuth, async (req, res) => {
   }
 });
 
+// Creating a new job record
 router.post('/jobs/new', withAuth, async (req, res) => {
     try {
       const newJob = await Job.create({
-  
+        title: req.body.jobTitle,
+        description: req.body.jobDescription,
+        user_id: req.session.user_id,
+        role_id: req.body.role_id
       });
   
       res.status(200).json(newJob);
-    } catch (err) {
+    } 
+    catch (err) {
       res.status(400).json(err);
     }
   });
+
+// Deleting a job record
+router.delete('/jobs/:id', withAuth, async (req, res) => {
+    try {
+        const deleteJob = await Blog.destroy({
+        where: {
+            id: req.params.id,
+        },
+      });
+
+    if (!deleteJob) {
+        res.status(404).json({ 
+            message: 'No blog found with this id!' 
+        });
+        return;
+    }
+
+    res.status(200).json(deleteJob);
+
+} catch (err) {
+    res.status(500).json(err);
+}
+});
   
