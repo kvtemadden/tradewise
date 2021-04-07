7d55d10768fb209b40326e23445e4af305d31219
kate <katy.madden99@gmail.com>
2021-04-07 21:10:05 +0100

    created functionality to: create comment, create post, view dashboard based on type of user, view single jobs

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -22,15 +22,40 @@ router.get('/signin', (req, res) => {
 // Dashboard route which shows user's own job postings (if any) || must be logged in.
 router.get('/dashboard', withAuth, async (req, res) => {
     try {
+        const user = await User.findOne({
+            where: {
+              id: req.session.user_id,
+            },
+          });
+
+        const checkCustomer = user.is_customer == 1 ? true : false;
+
         const userJobs = await Job.findAll({
+             include: [
+            {
+              model: User,
+              attributes: ['username', 'is_customer']
+            }],
             where: { user_id: req.session.user_id },
             order: [['date_created', 'ASC']],
         });
 
+        const allJobs = await Job.findAll({    
+            include: [
+            {
+              model: User,
+              attributes: ['username', 'is_customer']
+            }],
+            order: [['role_id', 'ASC'], ['date_created', 'ASC']],
+        });
+
         const myJobs = userJobs.map((job) => job.get({ plain: true }));
+        const otherJobs = allJobs.map((job) => job.get({ plain: true }));
 
         res.render('dashboard', {
             myJobs,
+            otherJobs,
+            checkCustomer,
             logged_in: req.session.logged_in,
         });
 
